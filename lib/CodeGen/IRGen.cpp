9684aa47eaad30371377e9f070bd8b58ccf39a75
eblasquezgonzalez <eblasquezgonzalez@quarkslab.com>
2025-09-06 14:46:55 +0200

    Finished tacky part

--- lib/CodeGen/IRGen.cpp
+++ lib/CodeGen/IRGen.cpp
@@ -61,6 +61,9 @@ ir::Value* IRGenerator::generateExpression(const Expr& Expr, ir::Function * IRFu
                 case UnaryOperator::UnaryOperatorKind::UopK_Complement:
                     Kind = ir::UnaryOp::UnaryOpKind::Complement;
                     break;
+                case UnaryOperator::UnaryOperatorKind::UopK_Not:
+                    Kind = ir::UnaryOp::UnaryOpKind::Not;
+                    break;
             }
             ir::Value * source = generateExpression(*UnaryOp.getExpr(), IRFunc);
             ir::UnaryOp * UnOp = Ctx.createUnaryOp(dynamic_cast<ir::Operand*>(source), Kind);
@@ -71,7 +74,9 @@ ir::Value* IRGenerator::generateExpression(const Expr& Expr, ir::Function * IRFu
         case Expr::Ek_BinaryOperator: {
             const auto& BinaryOp = dynamic_cast<const BinaryOperator&>(Expr);
 
-            ir::BinaryOp::BinaryOpKind Kind;
+            ir::BinaryOp::BinaryOpKind Kind = ir::BinaryOp::BinaryOpKind::none;
+            ir::ICmpOp::CmpOpKind CmpKind = ir::ICmpOp::CmpOpKind::none;
+
             switch (BinaryOp.getOperatorKind()) {
                 case BinaryOperator::BoK_Add:
                     Kind = ir::BinaryOp::BinaryOpKind::Add;
@@ -103,15 +108,106 @@ ir::Value* IRGenerator::generateExpression(const Expr& Expr, ir::Function * IRFu
                 case BinaryOperator::BoK_RightShift:
                     Kind = ir::BinaryOp::BinaryOpKind::Sar;
                     break;
+                case BinaryOperator::BoK_LowerThan:
+                    CmpKind = ir::ICmpOp::lt;
+                    break;
+                case BinaryOperator::BoK_LowerEqual:
+                    CmpKind = ir::ICmpOp::le;
+                    break;
+                case BinaryOperator::BoK_GreaterThan:
+                    CmpKind = ir::ICmpOp::gt;
+                    break;
+                case BinaryOperator::BoK_GreaterEqual:
+                    CmpKind = ir::ICmpOp::ge;
+                    break;
+                case BinaryOperator::Bok_Equal:
+                    CmpKind = ir::ICmpOp::eq;
+                    break;
+                case BinaryOperator::Bok_NotEqual:
+                    CmpKind = ir::ICmpOp::neq;
+                    break;
             }
             // According to C standard the subexpressions of the same operation
             // are usually unsequenced, they can be evaluated in any order.
             ir::Value * left = generateExpression(*BinaryOp.getLeft(), IRFunc);
             ir::Value * right = generateExpression(*BinaryOp.getRight(), IRFunc);
-            ir::BinaryOp *BinOp = Ctx.createBinaryOp(dynamic_cast<ir::Operand*>(left), dynamic_cast<ir::Operand*>(right), Kind);
-            if (IRFunc != nullptr)
-                IRFunc->add_instruction(BinOp);
-            return BinOp->getDestination();
+            if (Kind != ir::BinaryOp::BinaryOpKind::none) {
+                ir::BinaryOp *BinOp = Ctx.createBinaryOp(dynamic_cast<ir::Operand*>(left),
+                    dynamic_cast<ir::Operand*>(right), Kind);
+                if (IRFunc != nullptr)
+                    IRFunc->add_instruction(BinOp);
+                return BinOp->getDestination();
+            }
+            if (CmpKind != ir::ICmpOp::CmpOpKind::none) {
+                ir::ICmpOp *CmpOp = Ctx.createICmpOp(dynamic_cast<ir::Operand*>(left),
+                    dynamic_cast<ir::Operand*>(right), CmpKind);
+                if (IRFunc != nullptr)
+                    IRFunc->add_instruction(CmpOp);
+                return CmpOp->getDestination();
+            }
+
+            if (BinaryOp.getOperatorKind() == BinaryOperator::Bok_And) {
+                auto* false_label = Ctx.createNewLabel("false_label");
+                auto* end_label = Ctx.createNewLabel("end_label");
+                auto* result = Ctx.createReg();
+                auto* temp_left = Ctx.createReg();
+                auto* temp_right = Ctx.createReg();
+                auto* val_1 = Ctx.createInt(llvm::APSInt(llvm::APInt(32, abs(1))));
+                auto* val_0 = Ctx.createInt(llvm::APSInt(32, abs(0)));
+
+                auto* mov_left = Ctx.createCopy(left, temp_left);
+                auto* jump_if_zero_left = Ctx.createJZ(temp_left, false_label);
+                auto* mov_right = Ctx.createCopy(right, temp_right);
+                auto* jump_if_zero_right = Ctx.createJZ(temp_right, false_label);
+                auto* set_result_to_1 = Ctx.createCopy(val_1, result);
+                auto* jump_end = Ctx.createJump(end_label);
+                auto* set_result_to_0 = Ctx.createCopy(val_0, result);
+
+                if (IRFunc != nullptr) {
+                    IRFunc->add_instruction(mov_left);
+                    IRFunc->add_instruction(jump_if_zero_left);
+                    IRFunc->add_instruction(mov_right);
+                    IRFunc->add_instruction(jump_if_zero_right);
+                    IRFunc->add_instruction(set_result_to_1);
+                    IRFunc->add_instruction(jump_end);
+                    IRFunc->add_instruction(false_label);
+                    IRFunc->add_instruction(set_result_to_0);
+                    IRFunc->add_instruction(end_label);
+                }
+                return result;
+            }
+
+            if (BinaryOp.getOperatorKind() == BinaryOperator::Bok_Or) {
+                auto* false_label = Ctx.createNewLabel("false_label");
+                auto* end_label = Ctx.createNewLabel("end_label");
+                auto* result = Ctx.createReg();
+                auto* temp_left = Ctx.createReg();
+                auto* temp_right = Ctx.createReg();
+                auto* val_1 = Ctx.createInt(llvm::APSInt(llvm::APInt(32, abs(1))));
+                auto* val_0 = Ctx.createInt(llvm::APSInt(32, abs(0)));
+
+                auto* mov_left = Ctx.createCopy(left, temp_left);
+                auto* jump_if_not_zero_left = Ctx.createJNZ(temp_left, false_label);
+                auto* mov_right = Ctx.createCopy(right, temp_right);
+                auto* jump_if_not_zero_right = Ctx.createJNZ(temp_right, false_label);
+                auto* set_result_to_1 = Ctx.createCopy(val_1, result);
+                auto* jump_end = Ctx.createJump(end_label);
+                auto* set_result_to_0 = Ctx.createCopy(val_0, result);
+
+                if (IRFunc != nullptr) {
+                    IRFunc->add_instruction(mov_left);
+                    IRFunc->add_instruction(jump_if_not_zero_left);
+                    IRFunc->add_instruction(mov_right);
+                    IRFunc->add_instruction(jump_if_not_zero_right);
+                    IRFunc->add_instruction(set_result_to_1);
+                    IRFunc->add_instruction(jump_end);
+                    IRFunc->add_instruction(false_label);
+                    IRFunc->add_instruction(set_result_to_0);
+                    IRFunc->add_instruction(end_label);
+                }
+                return result;
+            }
+
         }
     }
     
